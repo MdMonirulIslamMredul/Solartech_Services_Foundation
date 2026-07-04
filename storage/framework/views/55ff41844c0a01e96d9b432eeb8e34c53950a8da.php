5eb3a7d19f3e6abc24a6939289bde09f64c3036a
Monirul Islam <monirulislammredul@gmail.com>
2026-07-05 00:43:49 +0600

    Refactor code structure for improved readability and maintainability

--- storage/framework/views/55ff41844c0a01e96d9b432eeb8e34c53950a8da.php
+++
@@ -1,8 +0,0 @@
-<form method="post" <?php echo e($attributes->merge(['action' => '#', 'class' => 'form-horizontal'])); ?>>
-    <?php echo csrf_field(); ?>
-    <?php echo method_field('patch'); ?>
-
-    <?php echo e($slot); ?>
-
-</form>
-<?php /**PATH C:\laragon\www\Solartech_Services_Foundation\resources\views/components/forms/patch.blade.php ENDPATH**/ ?>
